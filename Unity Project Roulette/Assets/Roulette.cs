e4916dd71a2b877784cdec99d1b96c4d64f1984c
Unknown <gurugurumegane56@gmail.com>
2018-05-21 17:00:21 +0900

    Roulette

--- Unity Project Roulette/Assets/Roulette.cs
+++ Unity Project Roulette/Assets/Roulette.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Roulette : MonoBehaviour {
+public class Roulette : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    float rotSpeed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        this.rotSpeed *= 0.96f;
+
+    }
 }
